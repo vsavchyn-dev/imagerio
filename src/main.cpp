09dbcab19eb8fa00bb2a205c9062fbe8a201245d
Vladyslav Savchyn <vsavchyn2005@gmail.com>
2024-12-02 23:02:26 -0500

    add: default files for compilation (with CMake o:wnly for now)

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <omp.h>
+#include <tbb/tbb.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Welcome to Imagerio!\n";
+
+    std::cout << "Here are CLI args to this tool:\n";
+    for (int i = 0; i < argc; i++) {
+        std::cout << "\t" << argv[i] << "\n";
+    }
+
+    // TODO: create a validation of CLI args to choose between different
+
+    // TODO: create a file reader for Images (stb_image.h library or smth)
+
+    // TODO: create switch (or just default pass) to one of the filters in filter directory
+
+    // TODO: create a file writer for images (default file out for now).
+    return 0;
+}
