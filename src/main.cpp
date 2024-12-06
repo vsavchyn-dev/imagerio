4d78ae80ab2420968548704295f7337835c05cc1
Vladyslav Savchyn <vsavchyn2005@gmail.com>
2024-12-06 15:12:36 -0500

    feat: finalized version of project

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include <omp.h>
-#include <tbb/tbb.h>
 #include <iostream>
 #include <string>
 #include "Image.h"
@@ -11,7 +9,21 @@ enum flags
     e, o, f, h, i, p
 };
 
+enum filters_enum
+{
+    gaussian_blur = 0,
+    grayscale,
+};
 
+const std::vector<std::string> valid_output_ext = {
+    ".png",
+    ".jpg",
+};
+
+const std::vector<std::string> valid_filters = {
+    "gaussian_blur",
+    "grayscale",
+};
 
 int main(int argc, char* argv[]) {
     std::cout << "Welcome to Imagerio!\n";
@@ -21,46 +33,138 @@ int main(int argc, char* argv[]) {
         std::cout << "\t" << argv[i] << "\n";
     }
 
-    // TODO: create a validation of CLI args to choose between different
-    std::string outputfile = "./";
-    std::string imagefile = "";
-    std::string filter = "";
-    bool earlyexit = false;
-
-    // TODO: Exit early if there are no args passed (for now it's good as we're testing things only)
-    for (int x = 1; x < argc; x++) {
-        int flag = starts_with("-o", argv[x]) * o +
-            (starts_with("-f", argv[x]) || starts_with("-filter", argv[x])) * f +
-            (starts_with("-h", argv[x]) || starts_with("-help", argv[x])) * h +
-            (starts_with("-i", argv[x]) || starts_with("-image", argv[x])) * i;
-        if (flag == 0 && x == 1) {
-            std::cout << "Invaild Input enter -h or -help if you need help\n";
-            earlyexit = true;
-            break;
-        }
-        switch (flag)
-        {
-        case o: outputfile.append(argv[x + 1]); break;
-        case f: filter.append(argv[x + 1]); break;
-        case i: imagefile.append(argv[x + 1]); break;
-        case h: std::cout 
+    if (argc == 1) {
+        std::cout << "No input file provided to program...\n"
             << "usage: imagerio  [<option>] [<input>] ... [<option>] [<input>] \n\n"
             << "The following options are available :\n\n"
-            << " -o or -output     name of the outfile image, this name must include the correct file extension \n"
-            << " -f or -filter     name of the filter that will be applied to the image  \n"
-            << " -h or -help       list of commmands \n"
-            << " -i or -image      image file name and path example: ./folder/image.png or C:\\Users\\WindowsUser\\Pictures\\image.png \n"
-            << " -p or -parllel    set the program to use multi-threading \n\n";
+            << "\t-o or -output     name of the outfile image, this name must include the correct file extension \n"
+            << "\t-f=<valid_filter> or -filter=<valid_filter>     name of the filter that will be applied to the image  \n"
+            << "\tsupported modes:\n"
+            << "\t\t gaussian_blur - blur image with gaussian blur\n"
+            << "\t\t grayscale     - make image gray\n"
+            << "\t-h or -help       list of commmands \n"
+            << "\t-i or -image      image file name and path example: ./folder/image.png or C:\\Users\\WindowsUser\\Pictures\\image.png \n"
+            << "\t-p or -parllel    set the program to use multi-threading \n\n";
+
+        return -1;
+    }
+
+    std::string outputfile = "";
+    std::string inputfile = "";
+    filters_enum filter = filters_enum::gaussian_blur;
+    bool earlyexit = false;
+    bool parallel_impl = false;
+
+    for (int x = 1; x < argc || earlyexit;) {
+        int flag = starts_with("-o", argv[x]) * o +
+            (starts_with(argv[x], "-f") || starts_with(argv[x], "-filter")) * f +
+            (starts_with("-h", argv[x]) || starts_with("-help", argv[x])) * h +
+            (starts_with("-i", argv[x]) || starts_with("-image", argv[x])) * i + 
+            (starts_with("-p", argv[x]) || starts_with("-parallel", argv[x])) * p;
+        
+        if (flag == 0) {
+            std::cout << "Invaild Input enter -h or -help if you need help\n";
             earlyexit = true;
+            
+            break;
+        }
+
+        switch (flag)
+        {
+        case o:
+            if (is_valid_path(argv[x + 1]) && is_valid_extension(argv[x + 1], valid_output_ext)) {
+                outputfile = argv[x + 1];
+                x += 2;
+            }
+            else {
+                std::cout << "Output path is invalid or extension is not one of possible ones!\n";
+                earlyexit = true;
+            }
+            break;
+        case f: 
+            if (is_valid_extension(argv[x], valid_filters)) {
+                const std::string filter_name_unparsed = argv[x];
+                size_t eq_sign_pos = filter_name_unparsed.find_first_of('='); 
+                const std::string filter_name = filter_name_unparsed.substr(eq_sign_pos + 1, filter_name_unparsed.size() - eq_sign_pos);
+
+                auto iter = std::find(valid_filters.begin(), valid_filters.end(), filter_name);
+                const size_t idx = std::distance(valid_filters.begin(), iter);
+                filter = filters_enum(idx);
+
+                x += 1;
+
+                // std::cout << "Unparsed name: " << filter_name_unparsed << "\n"
+                //         << "Parsed name: " << filter_name << "\n"
+                //         << "Position of eq sign: " << eq_sign_pos << "\n"
+                //         << "Calculated index: " << idx << "\n"
+                //         << "Final filter decision:" << filter << "\n";
+            }
+            else {
+                std::cout << "Invalid filter name!\n";
+                earlyexit = true;
+            }
+
+            break;
+        case i: 
+            if (is_valid_path(argv[x + 1]) && is_valid_extension(argv[x + 1], valid_output_ext)) {
+                inputfile.append(argv[x + 1]); 
+                x += 2;
+            }
+            else {
+                std::cout << "File is invalid!\n";
+                earlyexit = true;
+            }
+
+            break;
+        case p:
+            std::cout << "Using parallel impl\n";
+            parallel_impl = true;
+            x += 1;
+
+            break;
+        case h: 
+            std::cout 
+            << "usage: imagerio  [<option>] [<input>] ... [<option>] [<input>] \n\n"
+            << "The following options are available :\n\n"
+            << "\t-o or -output     name of the outfile image, this name must include the correct file extension \n"
+            << "\t-f or -filter     name of the filter that will be applied to the image  \n"
+            << "\t-h or -help       list of commmands \n"
+            << "\t-i or -image      image file name and path example: ./folder/image.png or C:\\Users\\WindowsUser\\Pictures\\image.png \n"
+            << "\t-p or -parllel    set the program to use multi-threading \n\n";
+            earlyexit = true;
+
             break;
         default:
+            std::cout << "Wrong argument - check -help for help!\n";
+            earlyexit = true;
+
             break;
         }
     }
     if (earlyexit) {
-        return 0;
+        return -1;
     }
 
+    imgr::Image og_img(inputfile);
+
+    og_img.print_stats();
+
+    // make decision based on filter
+    std::cout << "Filter: " << filter;
+    switch (filter) {
+        case filters_enum::gaussian_blur:
+            parallel_impl ? imgr::GaussianBlur::apply_gaussian_blur_parallel(og_img) : imgr::GaussianBlur::apply_gaussian_blur(og_img);
+            break;
+        case filters_enum::grayscale:
+            parallel_impl ? imgr::GrayScale::grayscaleImageParallel(og_img) : imgr::GrayScale::grayscaleImage(og_img);
+        default:
+            std::cout << "Unhandeled filter!!!! \n";
+            break;
+    }
+
+    og_img.write(outputfile);
+
+    /*
     // TODO: finish with validation in the image class for read and write. 
     imgr::Image img(imagefile);
     imgr::Image img_copy = img;
@@ -105,6 +209,5 @@ int main(int argc, char* argv[]) {
     img_copy.write("./exmp-blurred-parallel.jpg");
     img_gray.write("./exmp-gray.jpg");
     img_gray_para.write("./exmp-gray-parallel.jpg");
-
-    return 0;
+    */
 }
--- src/utils.h
+++ src/utils.h
@@ -1,4 +1,6 @@
+#include <cctype>
 #if __cplusplus < 202002L  // pre C++20
+#include <fstream>
 #include <string>
 
 static bool ends_with(const std::string& str, const std::string& suffix) {
@@ -32,5 +34,53 @@ static bool starts_with(const std::string& str, const char* prefix,
 
 static bool starts_with(const std::string& str, const char* prefix) {
     return starts_with(str, prefix, std::string::traits_type::length(prefix));
+}
+
+static bool is_valid_path(const std::string& path) {
+    bool flag = false;
+    // Check if path is empty
+    if (path.empty())
+        return false;
+
+    // Check for invalid characters in path
+    const std::string invalid_chars = "\\/:*?\"<>|";
+    if (std::any_of(path.begin(), path.end(), [&invalid_chars](char c) {
+            return invalid_chars.find(c) != std::string::npos;
+        }))
+        return false;
+
+    try {
+        std::filesystem::path fs_path(path);
+
+        // Check if path is absolute or relative
+        if (!fs_path.is_absolute() && !fs_path.is_relative())
+            return false;
+
+        return true;
+    } catch (...) {
+        return false;
+    }
+    return flag;
+}
+
+static bool is_valid_extension(
+    const std::string& path, const std::vector<std::string>& valid_extensions) {
+    // Check if path or valid_extensions are empty
+    if (path.empty() || valid_extensions.empty())
+        return false;
+
+    std::string local_path(path.length(), '\0');
+
+    for (size_t i = 0; i < path.length(); i++) {
+        local_path[i] = std::tolower(path[i]);
+    }
+
+    for (size_t i = 0; i < valid_extensions.size(); i++) {
+        if (ends_with(path, valid_extensions[i])) {
+            return true;
+        }
+    }
+
+    return false;
 }
 #endif
