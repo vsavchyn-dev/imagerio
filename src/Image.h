151bccf77ac1c9e544c42f2a52e524ca5569e459
Vladyslav Savchyn <vsavchyn2005@gmail.com>
2025-01-21 15:44:14 -0500

    add: added license to stb_image files

--- src/Image.h
+++ src/Image.h
@@ -1,3 +1,20 @@
+/*
+** Copyright 2025 Vladyslav Savchyn
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**    http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+**
+*/
+
 #pragma once
 
 #include <cmath>
@@ -6,9 +23,9 @@
 #define STBI_FREE(p) free(p)
 
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
+#include <stb/stb_image.h>
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb_image_write.h>
+#include <stb/stb_image_write.h>
 
 #include <algorithm>
 #include <cstddef>
